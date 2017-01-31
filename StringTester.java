d3fea50f4f14d6931ad2ac5e7ed3c1581468cfc9
cakraklio <cakraklio@linnmar.local>
2017-01-31 14:15:15 -0600

    Added Weather
    Created Git Repository

---
+++ StringTester.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class StringTester here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StringTester
+{
+        private String weatherCondition;
+        public class StringTester{
+        }
+         
+            
+            
+        
+        
+        
+        
+        //String substring(0,5);
+        //int L = int length();
+        //System.out.println(int l);
+    
+}
---
+++ SuperClass.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class SuperClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SuperClass
+{
+   public static void main() {
+   }
+}
